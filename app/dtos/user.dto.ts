ccc5496c51d723e47e50b34c955d473297a0d83b
Elton Salanic <elton.salanic@gmail.com>
2026-02-16 21:37:30 -0800

    refactor user dto

--- app/dtos/user.dto.ts
+++ app/dtos/user.dto.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { Timezones } from "@/app/types/enums";
 
 //Anonymous Users
-export const anonUserSchema = z.object({
+export const AnonUserSchema = z.object({
   fName: z.string().min(1, "First name is required"),
   lName: z.string().min(1, "Last name is required"),
   timezone: z.enum(Timezones, "Must be a valid timezone"),
 });
 
-export type CreateAnonUser = z.infer<typeof anonUserSchema>;
+export type CreateAnonUser = z.infer<typeof AnonUserSchema>;
