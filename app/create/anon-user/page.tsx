f3a6ede60e9f73010a1b1e0973041cf42ba45e77
Elton Salanic <elton.salanic@gmail.com>
2026-02-19 10:37:34 -0800

    layout change /create pages

--- app/create/anon-user/page.tsx
+++ app/create/anon-user/page.tsx
@@ -2,7 +2,7 @@ import AnonUserForm from "./AnonUserForm";
 
 export default function CreateEventPage() {
     return (
-        <div>
+        <div className="w-full">
             <p>Start by adding your name and timezone. You will be the host of the event.</p>
             <AnonUserForm />
         </div>
