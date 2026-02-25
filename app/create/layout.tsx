bb9c43310a2a2b85695b34ba4574eed7865ce69d
Elton Salanic <elton.salanic@gmail.com>
2026-02-25 14:11:01 -0800

    styling selected dates

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -1,14 +1,12 @@
 export default function CreateLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <main className="flex-1 w-screen flex flex-col items-center p-10">
+    <main className="flex-1 w-screen flex flex-col items-center p-2">
       <h1 className="text-secondary-foreground pb-10">Create Event</h1>
-      <div className="w-full md:w-[80%] lg:w-[60%]">
-        {children}
-      </div>
+      <div className="w-full md:w-[80%] lg:w-[60%]">{children}</div>
     </main>
-  )
+  );
 }
