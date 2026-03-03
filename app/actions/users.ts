cb5c97f1bdb1f7ab268083cc904505a13927b810
Elton Salanic <elton.salanic@gmail.com>
2026-03-03 13:44:42 -0800

    add User type and return upon user creation

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -3,10 +3,11 @@
 import { createClient } from "@/lib/supabase/server";
 import { AnonUserSchema, CreateAnonUser } from "@/app/dtos/user.dto";
 import { ActionResult } from "@/app/types/ActionResult";
+import { User } from "../types/User";
 
 
 
-export async function createAnonUser(formData: unknown): Promise<ActionResult<CreateAnonUser>> {
+export async function createAnonUser(formData: unknown): Promise<ActionResult<User>> {
   // Validate input with Zod
   const validationResult = AnonUserSchema.safeParse(formData);
   
@@ -38,10 +39,17 @@ export async function createAnonUser(formData: unknown): Promise<ActionResult<Cr
       };
     }
 
-    console.log("User created successfully!", data);
+    console.log("--User created successfully--", data.user);
 
     return {
       success: true,
+      // Returning user in case it's needed in the future
+      data: {
+        id: data.user?.id || "No User ID Returned By Supabase",
+        fName: data.user?.user_metadata.fName,
+        lName: data.user?.user_metadata.lName,
+        timezone: data.user?.user_metadata.timezone,
+      }
     };
   } catch (error) {
     return {
